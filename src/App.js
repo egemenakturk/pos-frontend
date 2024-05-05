0d5de65be34283dec4b3957aa940108bce0bca64
egemen akturk <egemen.akturk@mapaglobal.com>
2024-05-05 14:55:29 +0300

    initial commit without fail

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import {Checkbox, MantineProvider, NativeSelect} from '@mantine/core';
+import "@mantine/core/styles.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <MantineProvider>
+            <div className="App">
+
+                <Checkbox
+                    defaultChecked
+                    label="I agree to sell my privacy"
+                />
+                <NativeSelect
+                    data={[
+                        {
+                            group: 'Frontend libraries',
+                            items: [
+                                {label: 'React', value: 'react'},
+                                {label: 'Angular', value: 'angular'},
+                                {label: 'Vue', value: 'vue', disabled: true},
+                            ],
+                        },
+                        {
+                            group: 'Backend libraries',
+                            items: [
+                                {label: 'Express', value: 'express'},
+                                {label: 'Koa', value: 'koa'},
+                                {label: 'Django', value: 'django'},
+                            ],
+                        },
+                    ]}
+                />
+
+
+            </div>
+        </MantineProvider>
+    );
 }
 
 export default App;
